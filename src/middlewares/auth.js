aef039b425dcdecd2b31a5bb24d2aae4f9a6bac7
Elenadelatorre <elenadelatorre1806@gmail.com>
2024-05-16 09:51:44 +0200

    Arreglo en el auth.js

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -2,7 +2,7 @@ const User = require('../api/models/user');
 const { verifySign } = require('../utils/jwt');
 
 //Autenticación de usuarios:
-const authenticateUser = async (req, res, next) => {
+const isUser = async (req, res, next) => {
   try {
     const token = req.headers.authorization;
 
@@ -27,36 +27,13 @@ const authenticateUser = async (req, res, next) => {
   }
 };
 
-//Usuario normal:
-const isUser = async (req, res, next) => {
-  await authenticateUser(req, res, async () => {
-    try {
-    
-      if (req.user.rol === 'user') {
-        return next();
-      } else {
-        return res.status(400).json('No eres un usuario normal');
-      }
-    } catch (error) {
-      console.log(error);
-      return res.status(400).json('Error de autenticación');
-    }
-  });
-};
-
 //Administrador:
 const isAdmin = async (req, res, next) => {
   await authenticateUser(req, res, async () => {
-    try {
-      
-      if (req.user.rol === 'admin') {
-        return next();
-      } else {
-        return res.status(400).json('No eres Administrador');
-      }
-    } catch (error) {
-      console.log(error);
-      return res.status(400).json('Error de autenticación');
+    if (req.user.rol === 'admin') {
+      return next();
+    } else {
+      return res.status(400).json('No eres Administrador');
     }
   });
 };
